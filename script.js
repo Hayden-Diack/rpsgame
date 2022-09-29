f78ad67663c0e733458861c535aee5bb02b26779
SkysAreRed <skysarered2@gmail.com>
2022-09-29 17:10:23 +0200

    Finish playround function conditions

--- script.js
+++ script.js
@@ -19,15 +19,19 @@ function playRound(playerSelection, computerSelection) {
     // main computer function
     if (computerSelection == "rock") {
         console.log("Draw!")
+        return "Draw!";
     }
     else if (computerSelection == "paper") {
         console.log("You lose! Paper beats Rock");
+        return "You lose!";
     }
     else if (computerSelection == "scissors") {
         console.log("You Win! Rock beats Scissors");
+        return "You Win!";
     }
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
