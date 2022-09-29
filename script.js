7fc8fa5a3d501d9002901ce56b03ab4acf7a69a9
SkysAreRed <skysarered2@gmail.com>
2022-09-29 17:09:47 +0200

    Finish playRound function messages

--- script.js
+++ script.js
@@ -20,7 +20,14 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection == "rock") {
         console.log("Draw!")
     }
+    else if (computerSelection == "paper") {
+        console.log("You lose! Paper beats Rock");
+    }
+    else if (computerSelection == "scissors") {
+        console.log("You Win! Rock beats Scissors");
+    }
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
