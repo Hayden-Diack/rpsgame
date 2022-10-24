5f6e03b85e68eb332cd3cfd87bdc3fe57a6234cf
SkysAreRed <skysarered2@gmail.com>
2022-10-24 15:53:35 +0200

    Its working babyyyyyyyyyyy

---
+++ d
@@ -0,0 +1,113 @@
+[33mcommit 08746b0187ca48b2ce16336af983b4bfd5d7bad3[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/rps-ui[m[33m, [m[1;32mrps-ui[m[33m)[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Oct 20 15:45:04 2022 +0200
+
+    asd
+
+[33mcommit 9b2a6b2fb93c7dcdc55844ff7e4fb269d2dbece3[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Oct 20 15:41:39 2022 +0200
+
+    attempted gui
+
+[33mcommit 3913098ef13aa394e0fd4f93e6f5f35052534286[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Fri Sep 30 19:10:59 2022 +0200
+
+    How to do best of 5 is beyond me
+
+[33mcommit 35a5e47b06a3a05e4bfba228f80bd2aaae3438fe[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Fri Sep 30 18:42:05 2022 +0200
+
+    Add global score values, games played
+
+[33mcommit 79c75e668e0412a45cbcc5f6436aa6f0140349c9[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Fri Sep 30 18:39:59 2022 +0200
+
+    Delete computer choice, use getcompchoice
+
+[33mcommit af2eb7e013f8c06ac52b3cca7a94edf2e2efe6c3[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Fri Sep 30 17:41:32 2022 +0200
+
+    Finish playround function conditions
+
+[33mcommit 06ba87890826ca95a6f7aae33b3fae620c953938[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Fri Sep 30 17:35:22 2022 +0200
+
+    Add user prompt
+
+[33mcommit a9b0b8e2e88c8bd8e110182b4c6800347bce7d6c[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Fri Sep 30 17:19:46 2022 +0200
+
+    Expand if condition for playerwin
+
+[33mcommit ac0680caefa222403a4041f060e1bb21c7eb297e[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 18:11:27 2022 +0200
+
+    I have no idea at this point
+
+[33mcommit 006b1a64a5219753916aac80a23688f62b23abe7[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 17:20:07 2022 +0200
+
+    Added loop without gamefunc
+
+[33mcommit f78ad67663c0e733458861c535aee5bb02b26779[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 17:10:23 2022 +0200
+
+    Finish playround function conditions
+
+[33mcommit 7fc8fa5a3d501d9002901ce56b03ab4acf7a69a9[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 17:09:47 2022 +0200
+
+    Finish playRound function messages
+
+[33mcommit d8b2808444294d40195f5f1ec9022a1db94fd9ef[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 16:57:22 2022 +0200
+
+    Adjust computer decision making
+
+[33mcommit 771ff670b82c6e74ce5e1e83c817b43a2935b0ac[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 16:22:32 2022 +0200
+
+    Create random number and apply value
+
+[33mcommit 486c969d8a8536319116f8660dbdb515c610a975[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 15:15:16 2022 +0200
+
+    Wrote pseudocode
+
+[33mcommit 43f10c4dd2c766ddca23a9cc214c329b44987547[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 15:05:51 2022 +0200
+
+    Edit readme
+
+[33mcommit a074b1f904dee60b12c9d7ba5632e61990468ab4[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 15:05:25 2022 +0200
+
+    Edit readme
+
+[33mcommit 422b0022ac78f1f9d6641bd64fbc777ddd6e3301[m
+Author: SkysAreRed <skysarered2@gmail.com>
+Date:   Thu Sep 29 15:01:48 2022 +0200
+
+    Create empty files
+
+[33mcommit 12eadbf30a4b36cb9312ef0f1e4ba9f36b8b226c[m
+Author: Hayden <57100257+SkysAreRed@users.noreply.github.com>
+Date:   Thu Sep 29 14:50:31 2022 +0200
+
+    Initial commit
--- script.js
+++ script.js
@@ -1,103 +1,123 @@
+let tempCompChoice = {Value: ""}; // compChoice
+let getComputerChoice = 0; // compchoiceint
+let compChoice;
+let playerSelection; // player choice
+const buttons = document.querySelectorAll('.btn');
+
 let playerScore = 0;
 let computerScore = 0;
-let game = 0;
 
-let playerSelection;
+const output = document.querySelector("#output");
+output.textContent = "May the best player win!";
 
-function getComputerChoice() {
+const player = document.querySelector("#player-score");
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector("#computer-score");
+computer.textContent = `Computer Score: ${computerScore}`;
+
+function computerPlay() {
         // generate computer entries
     const choiceNum = Math.floor(Math.random()*3) + 1;
     if (choiceNum == "1") {
-        console.log("computer has chosen");
-        return "rock";
+        tempCompChoice.Value = "rock";
+        return choiceNum;
     }
     else if (choiceNum == "2") {
-        console.log("computer has chosen");
-        return "paper";
+        tempCompChoice.Value = "paper";
+        return choiceNum;
     }
-    else { 
-        console.log("computer has chosen");
-        return "scissors";
+    else if (choiceNum == "3") { 
+        tempCompChoice.Value = "scissors";
+        return choiceNum;
     }
+    return choiceNum;
 }
 
+
+
 // buttons
-const paperbtn = document.querySelector('#paperbtn');
-paperbtn.addEventListener('click', () => {
-    playRound(playerSelection = 'paper');
-
-    return
+buttons.forEach((button)=>{button.addEventListener('click',()=>{
+    playerSelection = button.id;
+    console.log(playerSelection);
+    if (playerSelection == "rock"){
+        // rock
+    }
+    else if (playerSelection == "paper") {
+        // paper
+    }
+    else if (playerSelection == "scissors") {
+        // scissors
+    }
+    computerSelection = computerPlay(tempCompChoice);
+    playGame();
+    })
 });
 
-const scissorsbtn = document.querySelector('#scissorsbtn');
-scissorsbtn.addEventListener('click', () => {
-    playRound(playerSelection = 'scissors');
-
-    return
-});
-
-const rockbtn = document.querySelector('#rockbtn');
-rockbtn.addEventListener('click', () => {
-    playRound(playerSelection = 'rock');
-
-    return
-});
 
 // playRound
-function playRound(playerSelection) {
-    console.log(playerSelection);
-       
+function playRound() {
     let playerSelectionString = String(playerSelection);
     let playerSelectionLowercase = playerSelectionString.toLowerCase();
+    
     // If player chooses rock
     if (playerSelectionLowercase === "rock") {
-        if (computerSelection === "rock") {
+        if (tempCompChoice.Value === "rock") {
             console.log("DRAW!");
+            output.textContent = `Its a tie! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
             return "DRAW!" && console.log(playerScore, computerScore);
         }
-        else if (computerSelection === "paper") {
+        else if (tempCompChoice.Value === "paper") {
             console.log("LOSE!");
+            output.textContent = `You lost! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
             computerScore++;
             return "LOSE!" && console.log(playerScore, computerScore);
         }
         else {
             console.log("WIN!");
+            output.textContent = `You won! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
             playerScore++;
             return "WIN!" && console.log(playerScore, computerScore);
         }
     }
     // if player chooses paper
     else if (playerSelectionLowercase === "paper") {
-        if (computerSelection === "rock") {
+        if (tempCompChoice.Value === "rock") {
             console.log("WIN!");
+            output.textContent = `You won! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
             playerScore++;
             return "WIN!" && console.log(playerScore, computerScore);
         }
-        else if (computerSelection === "paper") {
+        else if (tempCompChoice.Value === "paper") {
             console.log("DRAW!");
+            output.textContent = `Its a tie! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
             return "DRAW!" && console.log(playerScore, computerScore);
         }
         else {
             console.log("LOSE!");
+            output.textContent = `You lost! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
             computerScore++;
             return "LOSE!" && console.log(playerScore, computerScore);
         }
     }
     // if player chooses scissors
     else if (playerSelectionLowercase === "scissors") {
-        if (computerSelection === "rock") {
+        if (tempCompChoice.Value === "rock") {
             console.log("LOSE!");
+            output.textContent = `You lost! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
             computerScore++;
             return "LOSE!" && console.log(playerScore, computerScore);
         }
-        else if (computerSelection === "paper") {
+        else if (tempCompChoice.Value === "paper") {
             console.log("WIN!");
+            output.textContent = `You won! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
             playerScore++;
             return "WIN!" && console.log(playerScore, computerScore);
         }
         else {
             console.log("DRAW!");
-            return "DRAW!" && console.log(playerScore, computerScore);
+            output.textContent = `Its a tie! You chose ${playerSelectionLowercase} and The computer chose ${tempCompChoice.Value}`;
+            return "DRAW!";
         }
     }
     // if player inputs something silly
@@ -107,5 +127,23 @@ function playRound(playerSelection) {
     }
 }
 
-const computerSelection = getComputerChoice();
-console.log(playerScore, computerScore);
+function playGame() {
+    output.textContent = "Choose Rock, Paper or Scissors";
+    playRound();
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
+    if (playerScore == 5) {
+        output.textContent = "You won the game!";
+        playerScore = 0;
+        computerScore = 0;
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${computerScore}`;
+    }
+    else if (computerScore == 5) {
+        output.textContent = "You lost the game!";
+        playerScore = 0;
+        computerScore = 0;
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${computerScore}`;
+    }
+}
